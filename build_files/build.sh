0c7eeae8a13088a14474ccc335b5c74c4da37cc5
foxsys <10726026+0xf24@users.noreply.github.com>
2025-08-24 22:34:33 -0400

    initial setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,24 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# repos
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+rpm --import https://pkgs.tailscale.com/stable/fedora/repo.gpg
+
 # this installs a package from fedora repos
 dnf5 install -y tmux 
+
+# desktop
+dnf5 -y install niri gdm xwayland-satellite
+
+# desktop portals
+dnf5 -y install xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring
+
+# utils
+dnf5 -y distrobox micro tailscale fuzzel podman
+
+# apps
+dnf5 -y install firefox nautilus kitty mpv 1password
 
 # Use a COPR Example:
 #
